03cbe1589af1d23c032348a4507dfb54ce8e0994
sid200307 <siddharthdoshi53@gmail.com>
2026-05-31 13:48:16 +0530

    added a .env file

--- consumer.go
+++ consumer.go
@@ -4,20 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/smtp"
+	"os"
 	"sync"
 	"time"
 )
 
 func emailworker(id int, ch chan Recipient, wg *sync.WaitGroup) {
 	defer wg.Done()
-	//sending real images 
+	//sending real images
 	// Gmail SMTP settings
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort := os.Getenv("SMTP_PORT")
 
-	// Your Gmail credentials
-	from := "siddharthdoshi53@gmail.com"
-	appPassword := "cfbt pmjk ztsl vovy"
+	//Gamil Crendentials
+	from := os.Getenv("EMAIL_FROM")
+	appPassword := os.Getenv("EMAIL_APP_PASSWORD")
 
 	auth := smtp.PlainAuth(
 		"",
@@ -27,25 +28,24 @@ func emailworker(id int, ch chan Recipient, wg *sync.WaitGroup) {
 	)
 
 	//Testing Service Mailpit
-		// smtpHost := "localhost"
-		// smtpPort := "1025"
-		// formatedmsg := fmt.Sprintf(
-		// 		"To:%s\r\n"+"Subject: Test Email \r\n\r\n%s\r\n",
-		// 		recipent.Email,
-		// 		"Just testing out Testing email Campagin",
-		// 	)
+	// smtpHost := "localhost"
+	// smtpPort := "1025"
+	// formatedmsg := fmt.Sprintf(
+	// 		"To:%s\r\n"+"Subject: Test Email \r\n\r\n%s\r\n",
+	// 		recipent.Email,
+	// 		"Just testing out Testing email Campagin",
+	// 	)
 
-		// msg := []byte(formatedmsg)
+	// msg := []byte(formatedmsg)
 
 	for recipent := range ch {
-		
-		msg,err2:=executeTemplate(recipent)
-		if err2!=nil{
-			fmt.Printf("Worker :%d Error Pasring template for %s",id,recipent.Email)
+
+		msg, err2 := executeTemplate(recipent)
+		if err2 != nil {
+			fmt.Printf("Worker :%d Error Pasring template for %s", id, recipent.Email)
 			//add to dlq
 			continue
 		}
-
 
 		fmt.Printf("Worker-%d: Sending email to %s\n", id, recipent.Email)
 		//using auth sending real mails
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sid200307/mailchimp
 
 go 1.26.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
